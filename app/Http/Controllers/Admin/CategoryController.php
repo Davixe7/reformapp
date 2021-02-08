ee8643fbd9679265cf655a3c2df6241f4d4f2b7f
N3r1 <mariajoseguilarte@gmail.com>
2021-02-08 01:56:39 -0400

    Controller for Category CRUD created, Form Requests for Category added too

---
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use Illuminate\Http\Request;
+use App\Http\Requests\StoreCategoryRequest;
+use App\Http\Requests\UpdateCategoryRequest;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $Categories = Category::all();
+        return view('admin.categories.index', ['categories' => $categories]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin.categories.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreCategoryRequest $request)
+    {
+        $category = Category::create([
+            'name' => $request->name,
+            'email' => $request->icon_name
+        ]);
+
+        if( !$category ){return false;}
+
+        return view('admin.categories.show', ['category' => $category]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Category $category)
+    {
+        return view('admin.categories.show', ['category' => $category]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Category $category)
+    {
+        return view('admin.categories.edit', ['category' => $category]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateCategoryRequest $request, Category $category)
+    {
+        $category->update([
+            'name' => $request->name ?: $category->name,
+            'icon_name' => $request->icon_name ?: $category->icon_name
+        ]);
+
+        return view('admin.categories.show', ['category' => $category]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Category $category)
+    {
+        $category->destroy();
+        return view('admin.categories.index', ['categories' => category::all()]);
+    }
+}
